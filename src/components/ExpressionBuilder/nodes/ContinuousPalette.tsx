e8b214f556fe1d4ce4f95224e53478ba94416a4c
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2024-07-05 20:28:54 -0500

    d3 continuous palettes

--- src/components/ExpressionBuilder/nodes/ContinuousPalette.tsx
+++ src/components/ExpressionBuilder/nodes/ContinuousPalette.tsx
@@ -6,6 +6,36 @@ import { useNodeData } from './useNodeData'
 import * as d3 from 'd3'
 
 const palettes = ['viridis']
+const SCHEMES = {
+  Blues: 'interpolateBlues',
+  Greens: 'interpolateGreens',
+  Greys: 'interpolateGreys',
+  Oranges: 'interpolateOranges',
+  Purples: 'interpolatePurples',
+  Reds: 'interpolateReds',
+  Turbo: 'interpolateTurbo',
+  Viridis: 'interpolateViridis',
+  Inferno: 'interpolateInferno',
+  Magma: 'interpolateMagma',
+  Plasma: 'interpolatePlasma',
+  Cividis: 'interpolateCividis',
+  Warm: 'interpolateWarm',
+  Cool: 'interpolateCool',
+  CubehelixDefault: 'interpolateCubehelixDefault',
+  BuGn: 'interpolateBuGn',
+  BuPu: 'interpolateBuPu',
+  GnBu: 'interpolateGnBu',
+  OrRd: 'interpolateOrRd',
+  PuBuGn: 'interpolatePuBuGn',
+  PuBu: 'interpolatePuBu',
+  PuRd: 'interpolatePuRd',
+  RdPu: 'interpolateRdPu',
+  YlGnBu: 'interpolateYlGnBu',
+  YlGn: 'interpolateYlGn',
+  YlOrBr: 'interpolateYlOrBr',
+  YlOrRd: 'interpolateYlOrRd',
+}
+const SCHEME_NAMES = Object.keys(SCHEMES)
 
 export const ContinuousPalette = memo(({ data, isConnectable }) => {
   const nodeId = useNodeId()
@@ -15,46 +45,48 @@ export const ContinuousPalette = memo(({ data, isConnectable }) => {
   const setNodeData = useSetNodeData()
 
   const handleChange = useCallback(event => {
-    setNodeData('palette', event.target.value)
+    setNodeData('scheme', event.target.value)
   }, [])
 
   const nodeData = useNodeData()
 
   return (
     <Combination
-      inputs={['value', 'min', 'mean', 'max']}
+      inputs={['value', 'min', 'center', 'max']}
       getExpression={inputExpressions => {
         const min = Number(inputExpressions.min)
-        const mean = Number(inputExpressions.mean)
+        const center = Number(inputExpressions.center)
         const max = Number(inputExpressions.max)
+
+        const schemeFnName = SCHEMES[nodeData.scheme]
+
+        const fn = d3[schemeFnName]
 
         const exp = [
           'interpolate',
           ['linear'],
           inputExpressions.value,
           min,
-          ['to-color', d3.interpolateViridis(0)],
-          (mean + min) / 2,
-          ['to-color', d3.interpolateViridis(0.25)],
-          mean,
-          ['to-color', d3.interpolateViridis(0.5)],
-          (mean + max) / 2,
-          ['to-color', d3.interpolateViridis(0.75)],
+          ['to-color', fn(0)],
+          (center + min) / 2,
+          ['to-color', fn(0.25)],
+          center,
+          ['to-color', fn(0.5)],
+          (center + max) / 2,
+          ['to-color', fn(0.75)],
           max,
-          ['to-color', d3.interpolateViridis(1)],
+          ['to-color', fn(1)],
         ]
-
-        console.log(exp)
 
         return exp
       }}
       data={data}
       isConnectable={isConnectable}
     >
-      <select name="property" id="property" onChange={handleChange}>
-        {palettes.map(palette => (
-          <option key={palette} value={palette}>
-            {palette}
+      <select name="property" id="property" value={nodeData.scheme} onChange={handleChange}>
+        {SCHEME_NAMES.map(scheme => (
+          <option key={scheme} value={scheme}>
+            {scheme}
           </option>
         ))}
       </select>
@@ -66,5 +98,5 @@ ContinuousPalette.getDefaultNode = () => ({
   id: `${+new Date()}`,
   type: 'continuousPalette',
   position: { x: 0, y: 0 },
-  data: { palette: 'viridis' },
+  data: { scheme: 'Viridis' },
 })
