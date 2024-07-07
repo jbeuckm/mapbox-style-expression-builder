0910bd9e4d3ee73f8738d2b92c1e44f59ca74e90
Joe Beuckman <jbeuckman@lm-jbeuckman.home>
2024-07-07 01:08:46 -0500

    regen dist

--- dist/types.d.ts
+++
@@ -1,8 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-
-type ExpressionBuilderProps = {
-    properties: string[];
-};
-declare const ExpressionBuilder: ({ properties }: ExpressionBuilderProps) => react_jsx_runtime.JSX.Element;
-
-export { ExpressionBuilder };
